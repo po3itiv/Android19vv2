e01afb661eca83bc80dd011fc84687556c24dd9f
po3itiv <po3itiv01@mail.ru>
2022-10-24 15:07:25 +0300

    Laba3Add

---
+++ Lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.lab3;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    // Объявляем об использовании следующих объектов:
+    private EditText username;
+    private EditText password;
+    private TextView attempts;
+    private TextView numberOfAttempts;
+
+    // Число для подсчета попыток залогиниться:
+    int numberOfRemainingLoginAttempts = 999;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Связываемся с элементами нашего интерфейса:
+        username = (EditText) findViewById(R.id.edit_user);
+        password = (EditText) findViewById(R.id.edit_password);
+        attempts = (TextView) findViewById(R.id.attempts);
+        numberOfAttempts = (TextView) findViewById(R.id.number_of_attempts);
+        numberOfAttempts.setText(Integer.toString(numberOfRemainingLoginAttempts));
+
+    }
+
+    // Обрабатываем нажатие кнопки "Войти":
+    @SuppressLint("SetTextI18n")
+    public void Login(View view) {
+
+        // Если введенные логин и пароль будут словом "admin",
+        // показываем Toast сообщение об успешном входе:
+        if (username.getText().toString().equals("oleg") &&
+                password.getText().toString().equals("oleg")) {
+            Toast.makeText(getApplicationContext(), "Вход выполнен!",Toast.LENGTH_SHORT).show();
+
+            // Выполняем переход на другой экран:
+            Intent intent = new Intent(MainActivity.this,Second.class);
+            startActivity(intent);
+        }
+
+        // В другом случае выдаем сообщение с ошибкой:
+        else {
+            Toast.makeText(getApplicationContext(), "Неправильные данные!",Toast.LENGTH_SHORT).show();
+            numberOfRemainingLoginAttempts--;
+
+            // Делаем видимыми текстовые поля, указывающие на количество оставшихся попыток:
+            attempts.setVisibility(View.VISIBLE);
+            numberOfAttempts.setVisibility(View.VISIBLE);
+            numberOfAttempts.setText(Integer.toString(numberOfRemainingLoginAttempts));
+
+        }
+    }
+}
